65c5d03a7166697043d32c5949eb8a214885e9f5
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2015-10-18 12:55:50 +0200

    added start of chapter 3 of dbda

---
+++ R/dbda/C3.R
@@ -0,0 +1,47 @@
+x = seq( from = -2,
+         to = 2,
+         by = 0.1)
+
+y = x ^ 2
+
+plot (x,
+      y,
+      col = " skyblue",
+      type = "l")
+
+# ?plot
+# ?par
+# help.start()
+# ??plot
+# ?"??"
+
+(TRUE | FALSE) & FALSE
+
+all.equal(0.1*10/10, 0.1)
+
+v1 = c (3, 4.5)
+
+v
+
+v2 = c(4 , 5)
+
+v2
+
+v1 * v2
+
+1:3^3
+
+s = seq( to = 2,
+         by = 0.1,
+         length.out = 100
+         )
+
+s
+
+rep(v1, times =2)
+rep(v1, times = c(4,2))
+rep (v1, each = 2, length = 20)
+
+rep( v2, each=2, times=c(1,2,7,1) )
+
+
