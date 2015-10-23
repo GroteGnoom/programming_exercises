d2298745fdd9e09f74d7c30cf02892b97019d1ce
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2015-10-23 19:18:01 +0200

    simple svg drawing

--- elm/game/Blob.elm
+++ elm/game/Blob.elm
@@ -1,8 +1,9 @@
 module Blob where
 
 import Effects exposing (Effects, Never)
-import Html exposing (..)
-
+import Html exposing (Html)
+import Svg exposing (svg, rect, g, text, polygon)
+import Svg.Attributes exposing (..)
 
 
 -- MODEL
@@ -34,8 +35,13 @@ update action model =
 
 view : Signal.Address Action -> Model -> Html
 view address model =
-  div []
-    [ text "Hellooboo" ]
+--  div []
+--    [ text "Hellooboo" ]
+  svg [ version "1.1", x "0", y "0", viewBox "0 0 323.141 322.95" ]
+    [ polygon [ fill "#F0AD00", points "161.649,152.782 231.514,82.916 91.783,82.916" ] []
+    ]
+
+
 
 -- EFFECTS
 -- none :)
