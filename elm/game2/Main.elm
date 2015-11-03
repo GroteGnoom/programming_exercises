4d42657bf5ec7f5e8fc90288199c296cde425c0b
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2015-11-03 22:11:47 +0100

    view from a modelin elm game2

--- elm/game2/Main.elm
+++ elm/game2/Main.elm
@@ -2,10 +2,28 @@ import Graphics.Element exposing (container, middle, Element)
 import Graphics.Collage exposing (rect, collage, filled)
 import Color exposing (rgb)
 
-main = view
+-- MAIN
+main = view modelInit
 
-view : Element
-view = container 100 100 middle <|
-    collage 100 100
-        [rect 20 20 |> filled (rgb 100 100 100)]
+-- INIT
+
+modelInit = Model (Position 20 20)
+
+-- MODEL
+
+type alias Position =
+    { x : Float
+    , y : Float
+    }
+
+type alias Model =
+    {squarePosition : Position
+    }
+
+-- VIEW
+view : Model -> Element
+view model = container 500 500 middle <|
+    collage 500 500
+        [rect model.squarePosition.x model.squarePosition.y |> filled (rgb 100 100 100)]
+
 
