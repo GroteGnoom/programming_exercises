a4c60859693025b6cba17eb47e8335a70a12b725
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2015-11-03 22:57:26 +0100

    elm game2 moving up and down

--- elm/game2/Main.elm
+++ elm/game2/Main.elm
@@ -1,13 +1,27 @@
 import Graphics.Element exposing (container, middle, Element)
-import Graphics.Collage exposing (rect, collage, filled)
+import Graphics.Collage exposing (rect, collage, filled, move)
 import Color exposing (rgb)
-
--- MAIN
-main = view modelInit
+import Keyboard
+import Time exposing (fps, inSeconds)
 
 -- INIT
 
 modelInit = Model (Position 20 20)
+
+-- SIGNALS
+
+main = Signal.map view model
+
+model = Signal.foldp update modelInit input
+
+delta =
+  Signal.map inSeconds (fps 35)
+
+input : Signal Input
+input =
+  Signal.sampleOn delta <|
+    Signal.map Input
+      (Signal.map .y Keyboard.arrows)
 
 -- MODEL
 
@@ -15,6 +29,9 @@ type alias Position =
     { x : Float
     , y : Float
     }
+
+type alias Input =
+    { dir : Int}
 
 type alias Model =
     {squarePosition : Position
@@ -24,6 +41,22 @@ type alias Model =
 view : Model -> Element
 view model = container 500 500 middle <|
     collage 500 500
-        [rect model.squarePosition.x model.squarePosition.y |> filled (rgb 100 100 100)]
+        [rect 20 20 |>
+        filled (rgb 100 100 100) |>
+        move (model.squarePosition.x, model.squarePosition.y)
+        ]
 
+-- UPDATE
+
+update : Input -> Model -> Model
+update {dir} ({squarePosition} as model) =
+    let newSquarePosition = updateSquare dir squarePosition
+    in {model | squarePosition <- newSquarePosition}
+
+updateSquare : Int -> Position -> Position
+updateSquare dir ({x,y} as position) =
+    { position |
+        x <- x,
+        y <- y + toFloat dir
+    }
 
