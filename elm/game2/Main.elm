0ed38761021d970981a96c01c5946d79c92bfdae
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2015-11-06 11:14:56 +0100

    slightly more dry structure

--- elm/game2/Main.elm
+++ elm/game2/Main.elm
@@ -1,5 +1,5 @@
 import Graphics.Element exposing (container, bottomLeft, Element)
-import Graphics.Collage exposing (rect, collage, filled, move)
+import Graphics.Collage exposing (rect, collage, filled, move, Form)
 import Color exposing (rgb, Color)
 import Keyboard
 import Time exposing (fps, inSeconds)
@@ -65,31 +65,26 @@ type alias Model =
 view : Model -> Element
 view model = container 500 500 bottomLeft <|
     collage 500 500
-        [rect model.player.width model.player.height |>
-        filled model.player.color |>
-        move (model.player.left, model.player.bottom)
-        ,rect model.enemy.width model.enemy.height |>
-        filled model.enemy.color |>
-        move (model.enemy.left, model.enemy.bottom)
+        [drawSquare model.player
+        ,drawSquare model.enemy
         ]
 
 -- UPDATE
 
 update : Input -> Model -> Model
 update input ({player, enemy} as model) =
-    let newPlayer1 = updateSquare1 input player
-    in let newPlayer2 = updateSquare2 enemy newPlayer1
-        in {model | player <- newPlayer2}
+    let newPlayer = movePlayer input player |> hitToColorChange enemy
+    in {model | player <- newPlayer}
 
-updateSquare1 : Input -> Square -> Square
-updateSquare1 input ({left,bottom} as square) =
+movePlayer : Input -> Square -> Square
+movePlayer input ({left,bottom} as square) =
     { square |
         left <- left + ((toFloat input.arrows.x) * speed),
         bottom <- bottom + ((toFloat input.arrows.y) * speed)
     }
 
-updateSquare2 : Square -> Square -> Square
-updateSquare2 ({left,bottom, width, height, color} as enemy) ({left,bottom, width, height, color} as player) =
+hitToColorChange : Square -> Square -> Square
+hitToColorChange ({left,bottom, width, height, color} as enemy) ({left,bottom, width, height, color} as player) =
     if hitSquareSquare player enemy
         then
             { player |
@@ -108,3 +103,10 @@ hitSquareSquare square1 square2 =
     square1.left + square1.width > square2.left &&
     square1.bottom < square2.bottom + square2.height &&
     square1.bottom + square1.height > square2.bottom
+
+-- OTHER
+drawSquare : Square -> Form
+drawSquare square =
+    rect square.width square.height |>
+        filled square.color |>
+        move (square.left, square.bottom)
