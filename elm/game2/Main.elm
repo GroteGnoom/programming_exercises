7f5346199c903fba266887aa05083269520e00f1
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2015-11-06 10:50:29 +0100

    added collision detection

--- elm/game2/Main.elm
+++ elm/game2/Main.elm
@@ -1,6 +1,6 @@
-import Graphics.Element exposing (container, middle, Element)
+import Graphics.Element exposing (container, bottomLeft, Element)
 import Graphics.Collage exposing (rect, collage, filled, move)
-import Color exposing (rgb)
+import Color exposing (rgb, Color)
 import Keyboard
 import Time exposing (fps, inSeconds)
 
@@ -12,7 +12,9 @@ speed = 3
 -- INIT
 
 modelInit : Model
-modelInit = Model (Position 20 20)
+modelInit = { player = (Square 0 0 20 20 (rgb 100 100 100))
+            , enemy = (Square 100 100 20 20 (rgb 200 200 200))
+            }
 
 -- SIGNALS
 
@@ -31,7 +33,16 @@ input =
   Signal.sampleOn delta <|
     Signal.map Input Keyboard.arrows
 
+
 -- MODEL
+
+type alias Square =
+    { left : Float
+    , bottom : Float
+    , width : Float
+    , height : Float
+    , color : Color
+    }
 
 type alias Position =
     { x : Float
@@ -40,35 +51,60 @@ type alias Position =
 
 type alias Input =
     { arrows : 
-        {x : Int,
-         y : Int
+        { x : Int
+        , y : Int
         }
     }
 
 type alias Model =
-    {squarePosition : Position
+    { player : Square
+    , enemy : Square
     }
 
 -- VIEW
 view : Model -> Element
-view model = container 500 500 middle <|
+view model = container 500 500 bottomLeft <|
     collage 500 500
-        [rect 20 20 |>
-        filled (rgb 100 100 100) |>
-        move (model.squarePosition.x, model.squarePosition.y)
+        [rect model.player.width model.player.height |>
+        filled model.player.color |>
+        move (model.player.left, model.player.bottom)
+        ,rect model.enemy.width model.enemy.height |>
+        filled model.enemy.color |>
+        move (model.enemy.left, model.enemy.bottom)
         ]
 
 -- UPDATE
 
 update : Input -> Model -> Model
-update input ({squarePosition} as model) =
-    let newSquarePosition = updateSquare input squarePosition
-    in {model | squarePosition <- newSquarePosition}
+update input ({player, enemy} as model) =
+    let newPlayer1 = updateSquare1 input player
+    in let newPlayer2 = updateSquare2 enemy newPlayer1
+        in {model | player <- newPlayer2}
 
-updateSquare : Input -> Position -> Position
-updateSquare input ({x,y} as position) =
-    { position |
-        x <- x + ((toFloat input.arrows.x) * speed),
-        y <- y + ((toFloat input.arrows.y) * speed)
+updateSquare1 : Input -> Square -> Square
+updateSquare1 input ({left,bottom} as square) =
+    { square |
+        left <- left + ((toFloat input.arrows.x) * speed),
+        bottom <- bottom + ((toFloat input.arrows.y) * speed)
     }
 
+updateSquare2 : Square -> Square -> Square
+updateSquare2 ({left,bottom, width, height, color} as enemy) ({left,bottom, width, height, color} as player) =
+    if hitSquareSquare player enemy
+        then
+            { player |
+                color <- rgb 255 0 0
+            }
+        else 
+            { player |
+                color <- rgb 100 100 100
+            }
+
+-- COLLISION DETECTION
+
+hitSquareSquare : Square -> Square -> Bool
+hitSquareSquare square1 square2 =
+    square1.left < square2.left + square2.width &&
+    square1.left + square1.width > square2.left &&
+    square1.bottom < square2.bottom + square2.height &&
+    square1.bottom + square1.height > square2.bottom
